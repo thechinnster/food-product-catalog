a4c35e77de7fbd0ec688b8122830dad8bb381cf6
Ben Chinn <benchinn@Bens-MacBook-Pro.local>
2019-01-17 20:23:18 -0800

    Updates

---
+++ precache-manifest.3a1c30c3aacdf312d8d714cbda434777.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "31456b13393e00d7b366",
+    "url": "/food-product-catalog/static/css/main.5943b929.chunk.css"
+  },
+  {
+    "revision": "31456b13393e00d7b366",
+    "url": "/food-product-catalog/static/js/main.31456b13.chunk.js"
+  },
+  {
+    "revision": "5381c9da4ea11e96d4ba",
+    "url": "/food-product-catalog/static/js/1.5381c9da.chunk.js"
+  },
+  {
+    "revision": "c25bd1f8310b82e2214e",
+    "url": "/food-product-catalog/static/js/runtime~main.c25bd1f8.js"
+  },
+  {
+    "revision": "1678c5153934e40fae467ad1a6ce57b6",
+    "url": "/food-product-catalog/index.html"
+  }
+];
